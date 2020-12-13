d886276c730d45bc3541600850986437a69b550b
briancc101 <brianchambers876@gmail.com>
2020-12-13 17:59:33 -0500

    Removed status and init commands. Removed initialization checks from functions and commands.

--- telegramBot.js
+++ telegramBot.js
@@ -8,44 +8,16 @@ class TelegramBot {
 
         //URLs for http requests
         this.baseURL = `https://api.telegram.org/bot${teleToken}/`
-        this.updateURL = `${this.baseURL}getUpdates`
-
+        
         this.bot = new Telegraf(teleToken);
 
         this.initialzied = false;
-        
-        // allows bot to obtain chat id to send messages and files
-        this.bot.command('init', (ctx) =>{
-
-            try{
-                
-                this.chatID = ctx.chat.id
-                this.chatTitle = ctx.chat.title
-                
-                ctx.reply(`Bot Initialized for Chat: ${this.chatID}` )
-                this.initialzied = true;
-
-            } catch (err) {
-                console.log('Bot Initialization Error. Unable to obtain Chat ID')
-                ctx.reply('Bot Initialization Error. Unable to obtain Chat ID')
-            }
-            
-
-        })
-
-        this.bot.command('status', (ctx) =>{
-            if(this.initialzied){
-                ctx.reply(`Bot Initialized for Chat: ${this.chatID}`)
-            } else { 
-                ctx.reply('Bot uninitialized for this Chat. Run the /init command to initialize.')
-            }
-        })
 
         this.bot.command('help', (ctx) =>{
             const helpText = `You can control me by sending these commands:\n
-                                \t/init - initialize bot\n
-                                \t/status - check status of bot\n
-                                \t/gm <platform> <message> - sends text message to chosen platform\n`
+                                \t/gm <platform> <message> - sends text message to chosen platform\n
+                                \t/ls <platform> - lists members of all channels of chosen platform\n
+                                \t/file <platform> <file> - sends file to provided platform\n`
 
             ctx.reply(helpText)
         })
@@ -55,23 +27,21 @@ class TelegramBot {
         })
         
         this.bot.command('gm', async (ctx) => {
-            if(this.initialzied) {
+            this.sync(ctx)
 
-                if(this.onCommandReceived){
-                    const input = ctx.message.text.slice(1).trim().split(' ');
-                    const command = input.shift();
-                    const commandArgs = input.join(' ');
-                    const result = await this.onCommandReceived(command, commandArgs, 'telegram');
-                    console.log(result);
-                    return result;
-                }
-                
-            } else {
-                ctx.reply('Bot uninitialized. Run /init command to initialize.')
+            if(this.onCommandReceived){
+                const input = ctx.message.text.slice(1).trim().split(' ');
+                const command = input.shift();
+                const commandArgs = input.join(' ');
+                const result = await this.onCommandReceived(command, commandArgs, 'telegram');
+                console.log(result);
+                return result;
             }
         })
         
         this.bot.command('fs_l', async (ctx) => {
+            this.sync()
+
             if(this.initialzied)  {
                 if(this.onCommandReceived){
                     const input = ctx.message.text.slice(1).trim().split(' ');
@@ -90,6 +60,8 @@ class TelegramBot {
         })
 
         this.bot.command('fs_r', async (ctx) =>{
+            this.sync()
+
             if(this.initialzied)  {
                 if(this.onCommandReceived){
                     const input = ctx.message.text.slice(1).trim().split(' ');
@@ -107,32 +79,58 @@ class TelegramBot {
             }
         })
 
-        this.bot.command('test', (ctx) => {
+        //This listens for messages containing files and triggers the file sharing function if necessary
+        this.bot.use( async (ctx) => {
+            this.sync(ctx)
+
+            const update = ctx.update.message;
             
-            console.log(JSON.stringify(ctx, null, 2))
+            console.log(JSON.stringify(update, null, 2))
 
+            //Check if update object contains a photo, video or document file
+            //Check if update object contains a caption and caption entities to see if there is a command
+            if( (update.video || update.photo || update.document) && ((update.caption) && (update.caption_entities)) ){
+                if(update.caption_entities.type === "bot_command"){
+                    const caption = update.caption
+
+                    if(this.onCommandReceived){
+                        const input = caption.slice(1).trim().split(' ');
+                        const command = input.shift();
+                        const commandArgs = input.join(' ');
+                        const result = await this.onCommandReceived(command, commandArgs, 'telegram');
+                        console.log(result);
+                        return result;
+                    }
+
+                    if(update.video){
+                    console.log('VIDEO: ')
+                    console.log(JSON.stringify(update, null, 2)) 
+                    } else if(update.photo){
+                    console.log('PHOTO: ')
+
+                    const photo = update.photo
+                    console.log(JSON.stringify(update, null, 2))
+    
+                    } else if(update.document){
+                    console.log('DOCUMENT: ')
+                    console.log(JSON.stringify(update, null, 2))
+    
+                    }
+                }
+            }   
         })
 
-        this.bot.use((ctx) => {
-            this.chatID = ctx.chat.id
-            this.chatTitle = ctx.chat.title
-        })
+    }
 
+    //Syncs the bot with the current chat
+    sync(ctx) {
+        this.chatID = ctx.chat.id
+        this.chatTitle = ctx.chat.title
+        this.initialzied = true;
     }
 
     setCommandListener(commandListener) {
         this.onCommandReceived = commandListener;
-    }
-
-    async getUpdates(){
-
-        try{
-            const update = await axios.get(this.updateURL);
-            return update.data;  
-        } catch(err){
-            console.log(err)
-        }
-        
     }
 
     getChannels(){
@@ -143,33 +141,27 @@ class TelegramBot {
     }
 
     async sendMessageToAllChannels(message){
-        if(this.initialzied){
-            const response = await axios.post( `${this.baseURL}sendMessage?chat_id=${this.chatID}&text=${encodeURIComponent(message)}` );
-            if(response.status = 200){
-                console.log('Message sent successfully')
-            }
-        } else {
-            console.log('Chat Uninitialized. Run /init command in Telegram Client and try again.')
+        try{
+            const response = await this.bot.telegram.sendMessage(this.chatID, message)
+            return response
+        } catch (err) {
+            console.log(err)
         }
     }
 
-    async sendFileLocal(filePath){
-        if(this.initialzied){
-            const response = await this.bot.telegram.sendDocument(this.chatID, filePath)
-            return response
-        } else {
-            console.log('Chat Uninitialized. Run the /init command in the Telegram Client to initialize')
-        }
+    async sendFile(file){
+        const response = await this.bot.telegram.sendDocument(this.chatID, file)
+        return response
     }
 
-    async sendFileRemote(fileURL){
-        if(this.initialzied){
-            const response = await this.bot.telegram.sendDocument(this.chatID, fileURL);
-            return response
-        } else {
-            console.log('Chat Uninitialized. Run the /init command in the Telegram Client to initialize')
-        }
-    }
+    // async sendFileRemote(fileURL){
+    //     if(this.initialzied){
+    //         const response = await this.bot.telegram.sendDocument(this.chatID, fileURL);
+    //         return response
+    //     } else {
+    //         console.log('Chat Uninitialized. Run the /init command in the Telegram Client to initialize')
+    //     }
+    // }
 
     start(){
         return this.bot.launch();
